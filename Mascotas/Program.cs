caa968c120da91fc0c4b312faa3565327da6e8ea
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-01-29 19:15:10 -0400

    completado

--- Mascotas/Program.cs
+++ Mascotas/Program.cs
@@ -38,7 +38,31 @@ namespace Mascotas
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Mascotas y sus Dueños");
+            String dato = " ";
+            String nombreM, nombreD;
+            int edadM, edadD;
+
+            String NombreMascota, NombreDueño;
+            int EdadMascota, EdadDueño;
+
+            Console.WriteLine("Mascotas y sus Dueños\n");
+
+            Console.Write("Digite el Nombre de la Mascota: ");
+            NombreMascota = Console.ReadLine();
+            Console.Write("\nDigite la edad de la Mascota: ");
+            dato = Console.ReadLine();
+            EdadMascota = Convert.ToInt32(dato);
+
+            Console.Write("\nDigite el Nombre del Dueño: ");
+            NombreDueño = Console.ReadLine();
+            Console.Write("\nDigite la edad del Dueño: ");
+            dato = Console.ReadLine();
+            EdadDueño = Convert.ToInt32(dato);
+
+            DatosMascota dm = new DatosMascota(NombreMascota, EdadMascota, NombreDueño, EdadDueño);
+
+            Console.Write("\n\nLos datos:\n");
+            Console.Write("Mascota: {0} - {1}\nDueño: {2} - {3}", dm.NombreMascota, dm.EdadMascota, dm.DatosDueño.NombreDueño, dm.DatosDueño.EdadDueño);
         }
     }
 }
