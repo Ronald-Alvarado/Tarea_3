84670e2f1704566193f790f2f665c2a66d119cba
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-01-29 18:45:25 -0400

    Estructura lista

--- Mascotas/Program.cs
+++ Mascotas/Program.cs
@@ -4,9 +4,41 @@ namespace Mascotas
 {
     class Program
     {
+        public struct Dueño
+        {
+            public String NombreDueño;
+            public int EdadDueño;
+
+            public Dueño(String nombreD, int edadD)
+            {
+                NombreDueño = nombreD;
+                EdadDueño = edadD;
+            }
+
+        }
+        public struct DatosMascota
+        {
+            public String NombreMascota;
+            public int EdadMascota;
+
+            public Dueño DatosDueño;
+            
+
+            public DatosMascota(String nombreM, int edadM, String nombreD, int edadD)
+            {
+                NombreMascota = nombreM;
+                EdadMascota = edadM;
+
+                DatosDueño = new Dueño(nombreD,edadD);
+                
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mascotas y sus Dueños");
         }
     }
 }
