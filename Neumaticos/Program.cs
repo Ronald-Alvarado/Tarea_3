d7e9e226a6d286fd63090a66247c4c4268ce4a64
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-01-29 20:11:19 -0400

    Completado

--- Neumaticos/Program.cs
+++ Neumaticos/Program.cs
@@ -4,9 +4,23 @@ namespace Neumaticos
 {
     class Program
     {
+        enum Neumatico {diagonal, radial, asimétricos, direccionales, tubuless};
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            String dato = " ";
+            int numero;
+
+            Console.WriteLine("Tipos de Neumaticos\n");
+
+            Neumatico diag = Neumatico.diagonal;
+            Neumatico rad = Neumatico.radial;
+            Neumatico asi = Neumatico.asimétricos;
+            Neumatico dir = Neumatico.direccionales;
+            Neumatico tub = Neumatico.tubuless;
+
+            Console.WriteLine("Los tipos de Neumaticos son: {0}, {1}, {2}, {3}, {4}", diag, rad, asi, dir, tub);
         }
     }
 }
